9e9ca6f35b4436c0bdb082148553edcb58a35993
AndPhv <puhov2001@gmail.com>
2022-01-15 12:13:38 +0300

    Creation implements ActionListener and constructor

---
+++ src/com/company/TicTacToe.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe implements ActionListener // реализация прослушивателя действий
+{
+    TicTacToe()
+    {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
