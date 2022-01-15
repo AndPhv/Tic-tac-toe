c131689367263943939ff8381dfb0478948b4663
AndPhv <puhov2001@gmail.com>
2022-01-15 12:31:45 +0300

    Instantiation of classes and variable

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class TicTacToe implements ActionListener // реализация прослушивателя действий
 {
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
     TicTacToe()
     {
 
